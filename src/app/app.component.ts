72629eda0e5418a87515fce293e6b52ead576b54
Rijalul Fikri <fikri.desertlion@gmail.com>
2019-12-13 15:20:20 +0700

    Add qiscus sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import QiscusSDK from 'qiscus-sdk-core';
+
+const qiscus = new QiscusSDK();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'angular-qiscus-sdk';
+export class AppComponent implements OnInit {
+  title = 'initiating qiscus sdk ...';
+  
+  ngOnInit() {
+    const self = this;
+    qiscus.init({
+      AppId: 'sdksample',
+      options: {
+        loginSuccessCallback(data) {
+          console.log('successfully login', data);
+          self.title = `User ${data.user.username} has successfully login`;
+        }
+      }
+    })
+    qiscus.setUser('fikri@qiscus.com', 'password', 'Fikri');
+  }
 }
